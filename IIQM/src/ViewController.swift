c5b004debee3f01d30950055671747957dedee61
Rohan Patel <patelrohan@users.noreply.github.com>
2023-09-24 18:28:47 -0400

    Initial Git Commit

--- IIQM/src/ViewController.swift
+++ IIQM/src/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
 
         let iiqm = IIQM()
-        iiqm.calculate(path: Bundle.main.path(forResource: "data", ofType: "txt")!)
+        iiqm.calculate(path: Bundle.main.path(forResource: "data-20k", ofType: "txt")!)
     }
 
 
