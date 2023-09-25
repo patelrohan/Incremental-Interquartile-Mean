ef0f95090720129bcd1df87394cd85695e31c5d2
Rohan Patel <patelrohan@users.noreply.github.com>
2023-09-24 20:47:44 -0400

    Refactor: calculate function for better unit testing
    Refactored the 'calculate' function to improve its testability. The changes made enable more effective unit testing of the function's behavior.

--- Utility/IIQM.swift
+++ Utility/IIQM.swift
@@ -9,40 +9,48 @@
 import Foundation
 
 class IIQM {
+    
+
     func calculate(path: String) {
         let lines = FileReader.readFile(path: path)
         var data: [Int] = []
+        
         for line in lines {
             let value: Int = Int(line)!
             data.append(value)
             data.sort()
             
             if (data.count >= 4) {
-                let q: Double = Double(data.count) / 4.0
-                let i: Int = Int(q.rounded(.up)) - 1
-                let c: Int = Int((q*3).rounded(.down)) - i + 1
-                let ys = data[i...(i+c-1)]
-                let factor: Double = q - ((Double(ys.count) / 2.0) - 1)
-                var sum = 0
-                
-                var j = 0
-                for listEnumerator in ys {
-                    if (j == 0) {
-                        j += 1
-                        continue;
-                    }
-                    else if (j == (ys.count - 1)) {
-                        break;
-                    }
-                    
-                    sum += listEnumerator
-                    j += 1
-                }
-                
-                let mean: Double = (Double(sum) + Double(ys.first! + ys.last!) * factor) / (2.0 * q)
-                let meanString = String(format:"%.2f", mean)
+                let meanString = String(format:"%.2f", calculateIncrementalIQM(data: data))
                 print("Index => \(data.count), Mean => \(meanString)")
             }
         }
+    }
+    
+   
+    func calculateIncrementalIQM(data: [Int]) -> Double {
+        let q: Double = Double(data.count) / 4.0
+        let i: Int = Int(q.rounded(.up)) - 1
+        let c: Int = Int((q*3).rounded(.down)) - i + 1
+        let ys = data[i...(i+c-1)]
+        let factor: Double = q - ((Double(ys.count) / 2.0) - 1)
+        var sum = 0
+        
+        var j = 0
+        for listEnumerator in ys {
+            if (j == 0) {
+                j += 1
+                continue;
+            }
+            else if (j == (ys.count - 1)) {
+                break;
+            }
+            
+            sum += listEnumerator
+            j += 1
+        }
+        
+        let mean: Double = (Double(sum) + Double(ys.first! + ys.last!) * factor) / (2.0 * q)
+        return mean
     }
 }
